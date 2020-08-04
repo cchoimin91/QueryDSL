5374c36c96bf11d890552a443fecba6568c6d7ac
cchoimin91 <cchoimin1991@gmail.com>
2020-08-04 23:08:46 +0900

    queryDSL setting test

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,59 @@
+package study.querydsl;
+
+
+import com.mysema.commons.lang.Assert;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before(){
+        System.out.println("########################################");
+        queryFactory = new JPAQueryFactory(em); // 멀티스레드 환경에서도 동시성 이슈없이 설계되어짐
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamA);
+        Member member4 = new Member("member4", 40, teamA);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startQuerydsl(){
+        QMember m = new QMember("m");
+        Member member = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(member.getUsername()).isEqualTo("member1");
+    }
+}
