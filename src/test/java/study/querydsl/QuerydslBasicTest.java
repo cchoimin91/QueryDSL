fa98506680ca0a51e1300963baa22356519849cd
cchoimin91 <cchoimin1991@gmail.com>
2020-08-17 14:48:38 +0900

    concat

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -4,7 +4,9 @@ package study.querydsl;
 import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sun.xml.bind.v2.schemagen.xmlschema.AttrDecls;
@@ -410,6 +412,31 @@ public class QuerydslBasicTest {
                                 .when(member.age.loe(20)).then("20살 이상입니다")
                                 .otherwise("20살 이하입니다")
                 )
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void constant(){
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat(){
+        //stringValue() enum 처리할 때도 유용
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
                 .from(member)
                 .fetch();
 
