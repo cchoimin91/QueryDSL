50e5529eb1b90bbf49b3167542544ef823b0adc0
cchoimin91 <cchoimin1991@gmail.com>
2020-09-04 00:14:03 +0900

    paging

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,7 +1,11 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -48,5 +52,68 @@ public class MemberRepositoryImpl implements  MemberRepositoryCustom{
 
     private BooleanExpression teamNameEq(String teamName) {
         return StringUtils.hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    /**
+     * count 쿼리가 간단 할 때 fetchResult();
+     */
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+         QueryResults<MemberTeamDto> result = jpaQueryFactory
+                 .select(new QMemberTeamDto(
+                        member.id.as("memberId")
+                        , member.username
+                        , member.age
+                        , team.id.as("teamId")
+                        , team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUserName())
+                        , teamNameEq(condition.getTeamName())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults(); // count쿼리와, content 쿼리 같이 날림
+
+         List<MemberTeamDto> content = result.getResults();
+         long count = result.getTotal();
+
+         return new PageImpl<>(content, pageable,count);
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = jpaQueryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId")
+                        , member.username
+                        , member.age
+                        , team.id.as("teamId")
+                        , team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUserName())
+                        , teamNameEq(condition.getTeamName())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long count = jpaQueryFactory
+                .select(member)
+                .from(member)
+                //.leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUserName())
+                        , teamNameEq(condition.getTeamName())
+                )
+                .fetchCount();
+
+        return new PageImpl<>(content, pageable,count);
+
     }
 }
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
@@ -59,4 +62,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
         }
 
+    @Test
+    public void paging(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //검색조건
+        MemberSearchCondition condition = new MemberSearchCondition();
+
+        PageRequest pageRequest = PageRequest.of(0,3);
+
+        Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition,pageRequest);
+        Page<MemberTeamDto> result2 = memberRepository.searchPageComplex(condition,pageRequest);
+
+       assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result2.getSize()).isEqualTo(3);
+
     }
+
+}
